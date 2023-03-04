cd36eb85a195ba5e6e44d98904db1d568e928829
Alexander Luna <alexfox_2000@hotmail.com>
2023-03-04 09:59:57 -0500

    add validations to authdto

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,22 @@
-export interface AuthDto {
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+// cannot use validatoins in interface, must change to class
+// but interface and class is same
+export class AuthDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
   username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+  // we now need to tell nestjs to use validations pipes globally
 }
